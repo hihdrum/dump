665f89927e950c37d0a3028e678341f23af54998
makoto <makoto@insp-7380.elecom>
2021-08-13 21:10:53 +0900

    不要なコメントを削除した。

--- rust/dump.rs
+++ rust/dump.rs
@@ -2,13 +2,7 @@ use std::io;
 use std::io::Read;
 
 fn main() {
-
-//    let mut i : i32 = 0;
-
     for byte in io::stdin().bytes() {
-//        i = i + 1;
         print!("{:<02x} ", byte.unwrap());
     }
-
-//    print!("\nDBG: i = {}", i);
 }
