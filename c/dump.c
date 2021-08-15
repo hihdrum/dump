c40a474e588958644fabcb3692923ae70f711f01
makoto <88492711+hihdrum@users.noreply.github.com>
2021-08-15 23:46:48 +0900

    入力をバッファ化した。

--- c/dump.c
+++ c/dump.c
@@ -1,16 +1,37 @@
+#include <stdlib.h>
 #include <stdio.h>
+
+#define BUF_SIZE (1024)
 
 int main(void)
 {
+  char *readBuf = malloc(BUF_SIZE);
+  if(NULL == readBuf)
+  {
+    return 1;
+  }
+
   while(1)
   {
-    int ch = getchar();
-    if(EOF == ch)
+    size_t readSize = fread(readBuf, sizeof(char), BUF_SIZE, stdin);
+
+    char *readBufP = readBuf;
+    size_t i;
+    for(i = 0; i < readSize; i++)
     {
-      break;
+      printf("%02x ", (unsigned char)*readBufP);
+      readBufP++;
     }
 
-    printf("%02x ", ch);
+    if(BUF_SIZE > readSize)
+    {
+      if(feof(stdin))
+      {
+        return 0;
+      }
+
+      return 1;
+    }
   }
   
   return 0;
