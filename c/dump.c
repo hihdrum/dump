f636eac2b840efef357dc9e0a2fcab58fd83116a
makoto <88492711+hihdrum@users.noreply.github.com>
2021-08-17 00:21:11 +0900

    出力情報をバッファに貯めるようにした。

--- c/dump.c
+++ c/dump.c
@@ -11,16 +11,30 @@ int main(void)
     return 1;
   }
 
+  char *writeBuf = malloc(BUF_SIZE * 3);
+  if(NULL == writeBuf)
+  {
+    return 1;
+  }
+
   while(1)
   {
     size_t readSize = fread(readBuf, sizeof(char), BUF_SIZE, stdin);
 
     char *readBufP = readBuf;
+    char *writeBufP = writeBuf;
     size_t i;
     for(i = 0; i < readSize; i++)
     {
-      printf("%02x ", (unsigned char)*readBufP);
+      sprintf(writeBufP, "%02x ", (unsigned char)*readBufP);
       readBufP++;
+      writeBufP += 3;
+    }
+
+    size_t writeSize = fwrite(writeBuf, sizeof(char), 3 * readSize, stdout);
+    if((3 * readSize) > writeSize)
+    {
+      return 1;
     }
 
     if(BUF_SIZE > readSize)
